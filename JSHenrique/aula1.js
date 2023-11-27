14d9fa1622b5b0d4eb6de5c7048d635ffa616502
H3nr1qu <130712490+H3nr1qu@users.noreply.github.com>
2023-11-27 20:30:52 -0300

    Add files via upload

---
+++ JSHenrique/aula1.js
@@ -0,0 +1,24 @@
+//alert('ola mundo')
+//console.log('testando console.')
+//document.getElementById('titulo').innerHTML = 'olá pessoal'
+//document.write('testando algo;')
+
+// = atribui um valor a uma variavel
+// * multiplicaçao
+// /divisao
+// % resto da divisao
+// === comparador de igualdade com tipagem
+// == comparador de igualdade sem tipagem
+// !== comparador de desigualdade com tipagem
+// != comparador de desigualdade sem tipagem
+// && logico And
+// || logico OR
+
+clicks = 0
+
+function teste(){
+clicks +=1
+document.getElementById('contagem').innerHTML = 'Contagem De Clicks: ' +clicks
+
+}
+
