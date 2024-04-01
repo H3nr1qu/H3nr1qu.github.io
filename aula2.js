97433973d4538e4c852d0cd8f94b148fc7405518
H3nr1qu <130712490+H3nr1qu@users.noreply.github.com>
2024-04-01 20:02:12 -0300

    Add files via upload

---
+++ aula2.js
@@ -0,0 +1,48 @@
+//alert('ola mas dessa vez no arquivo')
+//frutas -['maça', 'uva','banana']
+
+//frutas- push('morago')
+//frutas [7]= 'pera'
+//frutas.pop()
+//frutas.shift()
+//frutas.splice(2,1)
+//frutas.splice(frutas.indexof('uva'),1)
+
+//frutas.sort().reverse()
+
+//document.write('lista completa'+frutas)
+//document.write('<br>posiçao especifica'+frutas())
+//document.write('<br>fruta especifica'+frutas.indexof('banana'))
+//document.write('<br>tamanho da lista'+frutas.length)
+mochila-[]
+
+function adicionar(){
+item= document.getElementById('Item').Value
+mochila.push(item)
+
+for(i=0;i<length; i++){
+html +-"<Div>"+mochila[i]+'<button>X</button></div>'
+    
+}
+
+
+
+document.getElementById('itens').innerHTML = mochila
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
